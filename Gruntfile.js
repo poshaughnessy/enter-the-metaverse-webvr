0daf72f61f742d41e409237d6074058b1cf0b288
Peter O'Shaughnessy <peter.oshaughnessy@pearson.com>
2014-11-27 12:11:36 +0000

    Minify CSS for deployment

--- Gruntfile.js
+++ Gruntfile.js
@@ -18,7 +18,7 @@ module.exports = function(grunt) {
 		cssmin: {
 			compress: {
 				files: {
-					'css/reveal.min.css': [ 'css/reveal.css' ]
+					'css/main.min.css': [ 'css/reveal.css', 'css/theme/futuretech.css', 'lib/css/zenburn.css' ]
 				}
 			}
 		},
